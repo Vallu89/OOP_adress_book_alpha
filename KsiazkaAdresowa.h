8eb4d97e9ece6750dd40f4e7f9e05d0488059e79
Rafał Zborowski <rafal.zborowski.dev@gmail.com>
2019-07-17 21:36:57 +0200

    Created elementary class

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaAdresowa{
+
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+    vector <Uzytkownik> uzytkownicy;
+
+public:
+    void rejestracjaUzytkownika();
+};
+
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include "KsiazkaAdresowa.h"
+
 
 using namespace std;
 
 
-
-
 int main(){
 
+    KsiazkaAdresowa ksiazkaAdresowa;
 
     return 0;
 }
